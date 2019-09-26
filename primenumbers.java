bb57fba668b57b191963f5d0580aa2df57d7afb3
Thomas <53497553+thomasandrew@users.noreply.github.com>
2019-09-26 14:20:14 -0300

    Add files via upload

---
+++ primenumbers.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Type a number: ");
+        int number = scan.nextInt();
+
+        for(int i=0;i<number/2;i++) {
+            
+        }
+
+
+
+
+
+
+
+
+    }
+}
