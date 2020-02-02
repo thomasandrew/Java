fe278de2c8ca0d8516ef51a281ac60f2f1706402
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-02 18:14:17 -0300

    Create Car.java

---
+++ OOP/classes/classespt1/Car.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Car {
+
+    private int doors; // Se for private só vai funcinar nessa classe.
+    private int wheels;
+    public String model; // Se for public vai funcinar em outra classe tambem.
+    private String engine;
+    private String colour;
+}
