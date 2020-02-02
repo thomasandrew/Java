dacd6154a22aa781c9a59925febfe5a6714f0ba6
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-02 18:50:46 -0300

    Create main.java

---
+++ OOP/classes/my_teacher_book/classespt1/main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car mygradebook = new Car();
+
+        mygradebook.displayMessage();
+    }
+}
+
+
