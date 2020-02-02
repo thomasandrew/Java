3157f10f09e847020b8961ffdea992f789ccdc3e
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-02 18:13:48 -0300

    Create main.java

---
+++ OOP/classes/classespt1/main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car(); // porsche is an Object.
+        Car holden = new Car();
+        porsche.model = "Carrera";
+    }
+}
+
