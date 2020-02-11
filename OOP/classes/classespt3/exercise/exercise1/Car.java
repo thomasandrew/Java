38450287540e3d52238a654035179890876b8945
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-11 14:30:50 -0300

    Create Car.java

---
+++ OOP/classes/classespt3/exercise/exercise1/Car.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Car {
+    private double width;
+    private double height;
+
+    public Car() {
+        this(0, 0);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        }
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        }
+        this.height = height;
+    }
+
+    public Car(double width, double height) {
+        if (width < 0 || height < 0) {
+            this.width = 0;
+            this.height = 0;
+        } else {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+}
