59963703364837c8c6db1d8b572a5028348d862f
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-05-20 22:08:58 -0300

    Create Main.java

---
+++ OOP/static/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator.printSum(5, 10);
+        printHello();
+    }
+
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+}
