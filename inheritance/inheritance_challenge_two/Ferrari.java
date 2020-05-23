46a077e1a883aeeb083d8ec1652a58379a156220
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-05-22 21:14:12 -0300

    Add files via upload

---
+++ inheritance/inheritance_challenge_two/Ferrari.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Ferrari extends Car {
+    private int roadServiceMonths;
+
+    public Ferrari(int roadServiceMonths) {
+        super("Ferrari", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0) {
+            stop();
+            changeGear(1);
+        } else if(newVelocity > 0 && newVelocity <= 10) {
+            changeGear(1);
+        } else if(newVelocity > 10 && newVelocity <= 20) {
+            changeGear(20);
+        } else if(newVelocity > 20 & newVelocity <= 30) {
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+        if (newVelocity > 0) {
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
+}
