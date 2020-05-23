ba3fe20b44045fe8954df8931823c75aa45ebafe
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-05-22 21:12:49 -0300

    Create Main.java

---
+++ inheritance/inheritance_challenge_two/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ferrari ferrari = new Ferrari(36);
+        ferrari.steer(45);
+        ferrari.accelerate(30);
+        ferrari.accelerate(20);
+        ferrari.accelerate(-42);
+    }
+}
