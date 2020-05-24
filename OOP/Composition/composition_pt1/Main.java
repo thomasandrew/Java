01db5303df356f24ff06be781670991c0de8814f
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-05-24 20:03:43 -0300

    Create Main.java

---
+++ OOP/Composition/composition_pt1/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440)); // I can create an Object inside the constructor parameter.
+
+        Motherboard theMotherboard = new Motherboard("JB-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+    }
+}
