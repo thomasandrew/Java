e2181e5ee2c3249a4e7a495d7db1e55d36b96c24
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-06-14 18:41:09 -0300

    Create Main.java

---
+++ OOP/polymorphism/Main.java
@@ -0,0 +1,108 @@
+package com.company;
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndenpendenceDay extends Movie {
+
+    public IndenpendenceDay() {
+        super("Indenpendence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Alien attempt to take over planet earth";
+    }
+}
+
+class Hercules extends Movie {
+
+    public Hercules() {
+        super("Hercules");
+    }
+
+    @Override
+    public String plot() {
+        return "Son of Zeus try to redeem himself";
+    }
+}
+
+class StarWars extends Movie {
+
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe";
+    }
+}
+
+class Forrgetable extends Movie {
+
+    public Forrgetable() {
+        super("Forgetable");
+    }
+
+    // No plot method.
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i +
+                    " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
+        }
+    }
+
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndenpendenceDay();
+
+            case 3:
+                return new Hercules();
+
+            case 4:
+                return new StarWars();
+
+            case 5:
+                return new Forrgetable();
+        }
+
+        return null;
+    }
+}
