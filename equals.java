9ac185d40a7a5d1b18018d3c49553d4e593eb1cf
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-07-04 14:34:18 -0300

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String num = "h";
+        if (num.equals("a")) {
+            System.out.println("yea");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+}
