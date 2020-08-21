8e525666bcbd63cdc3e7dd89499ea26fa84f40b4
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-08-21 15:06:19 -0300

    Create Main.java

---
+++ array/list_and_arraylist/list_and_arraylist_one/arraylist/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Main {
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    ArrayList<String> cars = new ArrayList<String>();
+
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+    System.out.println(cars.get(0));
+
+    // Change Item
+    cars.set(0, "Opel");
+    System.out.println(cars);
+
+    // Remove an Item
+    cars.remove(0);
+    System.out.println(cars);
+
+    // Remove all elements
+    // cars.clear();
+    System.out.println(cars);
+
+    // To find out how m many elements an ArrayList have
+    cars.size();
+    System.out.println(cars.size());
+
+    // Loop
+    for (int i = 0; i < cars.size(); i++) {
+      System.out.println(cars.get(i));
+    }
+
+    // for-each
+    for (String i : cars) {
+      System.out.println(i);
+    }
+
+    // Sort
+    Collections.sort(cars);
+    for (String i : cars) {
+      System.out.println(i);
+    }
+  }
+}
