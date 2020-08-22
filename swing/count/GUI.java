c58aade7d655b9ca4a8a65b3404f28abf26e3962
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-08-22 15:56:59 -0300

    Update GUI.java

--- swing/count/GUI.java
+++ swing/count/GUI.java
@@ -1,14 +1,24 @@
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+  private int count = 0;
+  private JLabel label;
+  private JFrame frame;
+  private JButton button;
+
   public GUI() {
-     JFrame frame = new JFrame("Example Frame");
+     frame = new JFrame("Example Frame");
      frame.setSize(700, 700);
      frame.setLayout(null);
 
-     JButton button = new JButton("Click me");
+     button = new JButton("Click me");
      button.setBounds(90, 200, 500, 100);
-     JLabel label = new JLabel("Number of clicks: 0");
+     button.addActionListener(this);
+
+     label = new JLabel("Number of clicks: 0");
      label.setBounds(50, 20, 190, 100);
 
      frame.add(button);
@@ -18,4 +28,10 @@ public class GUI {
      frame.setLocationRelativeTo(null);
      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   } 
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    count++;
+    label.setText("Number of clicks: " + count);
+  }
 }
