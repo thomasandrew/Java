1a3ec5996ebfe9414ba13bda42f12b0557efeabd
Thomas <53497553+thomasandrew@users.noreply.github.com>
2023-08-27 18:30:02 -0400

    Create main.java

---
+++ exercise_from_list/estruturas_de_selecao/exercise_two/main.java
@@ -0,0 +1,11 @@
+//Escreva um programa para ler três números inteiros distintos e determinar o menor deles.
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Pt_two pt_two = new Pt_two();
+    pt_two.inteiro();
+  }
+}
