94417f3bfaacbdf218ee46144bd1fa3aa6e5ef92
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-09-05 12:03:01 -0300

    Create Main.java

---
+++ college/aula_cinco/Main.java
@@ -0,0 +1,11 @@
+package model;
+
+import viewer.Janela_pessoa;
+import viewer.Janela_principal;
+
+public class Main {
+  public static void main(String[] args) {
+	  new Janela_principal();
+	  //new Janela_pessoa();
+  }
+}
