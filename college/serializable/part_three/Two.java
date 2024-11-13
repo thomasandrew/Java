6d1fe7d0e479e5dc4abb90e3c23e94ca082729a8
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-11-13 15:47:08 -0500

    Create Two.java

---
+++ college/serializable/part_three/Two.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.io.Serializable;
+
+public class Two {
+	private String name;
+	private Double price;
+	
+	public Two() {};
+	
+	public Two(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+}
