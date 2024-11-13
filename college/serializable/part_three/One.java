f6732f136edfb2905205fe2cbef25603909abd98
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-11-13 15:46:48 -0500

    Create One.java

---
+++ college/serializable/part_three/One.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.io.Serializable;
+
+public class One extends Two {
+	public One() {
+		super();
+	}
+	
+	public One(String name, Double price) {
+		super(name, price);
+	}
+}
