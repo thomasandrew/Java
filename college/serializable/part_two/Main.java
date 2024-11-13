691901333dc3c817fdde1f2ac4bb4cca93d94a90
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-11-13 15:32:02 -0500

    Create Main.java

---
+++ college/serializable/part_two/Main.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import model.One;
+
+public class Main {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	/*	One one = new One("Camiseta", 20d);
+		System.out.println(one); 
+		
+		ObjectOutputStream objectOutput = new ObjectOutputStream(new FileOutputStream("one1.byte"));
+		objectOutput.writeObject(one);
+		objectOutput.close();
+		
+		System.out.println("Writing file to: " + new java.io.File("one1.byte").getAbsolutePath()); */
+		
+	    ObjectInputStream objectInput = new ObjectInputStream(new FileInputStream("one1.byte"));
+		One one = (One) objectInput.readObject();
+		objectInput.close();
+		System.out.println(one.getName());
+	}
+}
