251710b8a2a212e48165f57ed2d32ef27f9b656f
Thomas <53497553+thomasandrew@users.noreply.github.com>
2025-08-06 11:54:57 -0300

    Create CtrlController.java

---
+++ college/Estrutura_de_dados/aula1/CtrlController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import util.ListaArray;
+
+public class CtrlPrograma {
+    public static void main(String[] args) {
+        ListaArray minhaLista = new ListaArray();
+        minhaLista.adicionar("primeiro");
+        minhaLista.adicionar("segundo");
+        minhaLista.adicionar("terceiro");
+        minhaLista.adicionar("quarto");
+    }
+}
